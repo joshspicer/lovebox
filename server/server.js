f85505ebccd6d5e7a24e10d586ecec94ff5887fc
Josh Spicer <hello@joshspicer.com>
2023-12-10 11:16:10 -0800

    Add 'ack' mechanism server-side to reduce the amount of client polling needed

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ const port = process.env.PORT || 3000;
 const enableDurationInSeconds = process.env.ENABLE_DURATION_IN_SECONDS || 5;
 
 const users = [
-    { id: "1", enable: 0 },
-    { id: "2", enable: 0 }
+    { id: "1", enable: 0, ack: 0 },
+    { id: "2", enable: 0, ack: 0 }
 ];
 
 const getRoute = (req, res, next) => {
@@ -22,31 +22,42 @@ const getRoute = (req, res, next) => {
         return next()
     }
 
-    res.json({ enable: userObj.enable, user: user });
+    if (userObj.enable === 1 && userObj.ack === 0) {
+        console.log(`[+] User '${userObj.id}' ack. Queuing disable in '${enableDurationInSeconds}' seconds.`);
+        userObj.ack = 1;
+        setTimeout(() => {
+            console.log(`[*] Disabling user ${userObj.id}`)
+            userObj.enable = 0;
+            userObj.ack = 0;
+        }, enableDurationInSeconds * 1000);
+    } else if (userObj.enable === 1 && userObj.ack === 1) {
+        console.log(`[no-op] User '${userObj.id}' is already active.`)
+    } else {
+        console.log(`[no-op] User '${userObj.id}' is not enabled.`);
+    }
+
+    res.json({ enable: userObj.enable, user: user, ack: userObj.ack });
     next();
 }
 
 const postRoute = (req, res, next) => {
     const user = req.query.user;
     if (!user) {
-        res.status(400).json({ error: 'Missing user query param' })
+        res.status(400).json({ error: 'missing user query param' })
         return next()
     }
 
     const userObj = users.find(u => u.id === user)
     if (!userObj) {
-        res.status(400).json({ error: 'User not found' })
+        res.status(400).json({ error: 'user not found' })
         return next()
     }
 
-    console.log(`** Enabling user ${userObj.id}`)
+    console.log(`[+] Enabling user '${userObj.id}'. Pending ack...`)
     userObj.enable = 1;
-    setTimeout(() => {
-        console.log(`** Disabling user ${userObj.id}`)
-        userObj.enable = 0;
-    }, enableDurationInSeconds * 1000);
+    userObj.ack = 0; // User needs to ack before we set timer for disabling
 
-    res.json({ enable: userObj.enable, user: user });
+    res.json({ enable: userObj.enable, user: user, ack: userObj.ack });
     next()
 }
 
