1c78321bfd902a1d1cbd5776c35949522fd9af50
Josh Spicer <hello@joshspicer.com>
2023-12-10 20:29:28 -0800

    Update variables.h

--- variables.h
+++ variables.h
@@ -1,6 +1,11 @@
 #include <Arduino.h>
 
-
 const char *ssid = "<TODO>";
 const char *password = "<TODO>";
-String serverPath = "http://<TODO>:3000/?user=1";
+String me = "1";
+String peer = "2";
+String baseAddress = "http://<TODO>:3000";
+
+String getMeAddress = baseAddress + "/?user=" + me;
+String provisionMeAddress = baseAddress + "/provision?user=" + me;
+String postPeerAddress = baseAddress + "/?user=" + peer;
